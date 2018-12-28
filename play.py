cb8a3f240dc3f2878303f1af7ac3bbfc73471b57
Niket Patel <niket_p@outlook.com>
2018-12-28 14:07:30 -0800

    Add display window to play.py

--- play.py
+++ play.py
@@ -4,14 +4,19 @@ import numpy as np
 from PIL import ImageGrab
 import keyboard
 
+cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+cv2.resizeWindow('image', 600, 600)
+
 model = load_model('final.h5')
 
 tick = 0
 while 1:
     scr = np.array(ImageGrab.grab(bbox=(175, 315, 775, 450)))
     cv2_scr = cv2.cvtColor(scr, cv2.COLOR_BGR2GRAY)
-    cv2_scr = cv2.resize(cv2_scr, (0,0), fx=0.2, fy=0.2)
+    cv2_scr = cv2.resize(cv2_scr, (0,0), fx=0.1, fy=0.1)
     cv2_scr = cv2.resize(cv2_scr, (32, 32))
+    cv2.imshow('image', cv2_scr)
+    cv2.waitKey(1)
     x = np.interp(cv2_scr, (cv2_scr.min(), cv2_scr.max()), (-1, +1))
     pred = (model.predict(x.reshape(1, 32, 32, 1)))
     if pred[0][1] > pred[0][0]:
@@ -23,3 +28,7 @@ while 1:
         print("wait")
         keyboard.release('up')
     tick += 1
+
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
+        break
